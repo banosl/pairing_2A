810100f73d68576127dd4d6cb922f6265e92b800
Leo Banos Garcia <bsleo625@gmail.com>
2022-09-07 19:20:04 -0600

    Add places_to_visit

--- friends.rb
+++ friends.rb
@@ -2,3 +2,7 @@ arrays = ["Joe", "Kate", "Jim", "Charlie", "Bob"]
 p arrays[2]
 p arrays[4]
 p arrays[0]
+
+places_to_visit = ["Alaska", "Italy", "China", "Canada", "Portland"]
+
+p places_to_visit.push("Seattle")
